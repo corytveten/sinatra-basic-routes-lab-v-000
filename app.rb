5e2dd04028e9bc1bda35aec4feff49ec67133eaa
Cory Tveten <corytveten@gmail.com>
2018-09-02 12:50:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,4 +5,12 @@ class App < Sinatra::Base
   get '/name' do
     "My name is Cory Tveten"
   end
+
+  get '/hometown' do
+    "My hometown is Redlands, CA"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'What a Fool Believes'"
+  end
 end
